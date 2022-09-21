dd8c13c4c69df74847184bbb9e439d7c300ff798
Ralph Mack <ralphmack@comcast.net>
2022-09-21 16:25:51 -0400

    Added support for editing and swapping attribute sets.

--- module/applications/item.mjs
+++ module/applications/item.mjs
@@ -13,12 +13,18 @@ export default class ItemSheetFudge extends ItemSheet {
         relativeTo: this.item
       }),
     });
+    if (item.type === "attributeset") {
+      context.attributelist = item.system.attributes.map( 
+        (attribute) => { return attribute.name;}
+      ).join('\n');
+    }
     return context;
   }  
 
   activateListeners(html) {
     super.activateListeners(html);
     html.find("#itemname").change(this._onNameChange.bind(this));
+    html.find("#attributes").change(this._onAttributesChange.bind(this));
     html.find("#skillgroup").change(this._onSkillGroupChange.bind(this));
     html.find("#odf").change(this._onNumberFieldChange.bind(this));
     html.find("#ddf").change(this._onNumberFieldChange.bind(this));
@@ -27,6 +33,24 @@ export default class ItemSheetFudge extends ItemSheet {
 
   async _onNameChange(event) {
     await this.object.update({name: event.target.value.trim()});
+  }
+
+  async _onAttributesChange(event) {
+    let newAttributeNames = event.target.value.split("\n");
+    let newAttributes = []
+    for (let name of newAttributeNames) {
+      let cleanName = name.trim();
+      if ( cleanName !== "") {
+        const attribute = { name: cleanName, level: 0 };
+        let oldAttribute = this.object.system.attributes.find ( 
+          (attribute) => attribute.name.toLowerCase() === cleanName.toLowerCase() );
+        if (oldAttribute) {
+          attribute.level = oldAttribute.level;
+        }
+        newAttributes.push(attribute);
+      }
+    }
+    await this.object.update( {"system.attributes": newAttributes })
   }
 
   async _onSkillGroupChange(event) {
--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -2,5 +2,28 @@
  * Extend the base Actor class to implement additional system-specific logic.
  */
  export default class ActorFudge extends Actor {
-    // Except we don't have any additional document-level logic yet ... but perhaps we will ...
+  /** @inheritdoc */
+  // This gets called before the new item gets created, so for unique items, we can remove the old one first.
+  async _onCreateEmbeddedDocuments(embeddedName, items, ...args) {
+    for (let item of items) {
+      if (item.type === "attributeset") {
+        let currentsets = this.items.filter((item) => item.type === "attributeset");
+        let oldids = currentsets
+          .filter((attrset) => attrset.id != item.id)
+          .map((attrset) => attrset.id);
+        if (oldids.length > 0) {
+          await this.deleteEmbeddedDocuments("Item", oldids);
+        }
+      } else if (item.type === "traitlevelset") {
+        let currentsets = this.items.filter((item) => item.type === "traitlevelset");
+        let oldids = currentsets
+          .filter((attrset) => attrset.id != item.id)
+          .map((attrset) => attrset.id);
+        if (oldids.length > 0) {
+          await this.deleteEmbeddedDocuments("Item", oldids);
+        }
+      }
+    }
+    await super._onCreateEmbeddedDocuments(embeddedName, items, ...args);
+  }
 }
